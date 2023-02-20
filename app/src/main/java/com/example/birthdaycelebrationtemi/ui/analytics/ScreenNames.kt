f6a838ad65e23abbfb89832cd6c75178320be92f
EsraaArify <earify@provensolution.com>
2023-02-20 12:07:43 -0800

    add new face request

--- app/src/main/java/com/example/birthdaycelebrationtemi/ui/analytics/ScreenNames.kt
+++ app/src/main/java/com/example/birthdaycelebrationtemi/ui/analytics/ScreenNames.kt
@@ -2,16 +2,6 @@ package com.example.birthdaycelebrationtemi.ui.analytics
 
 
 enum class ScreenNames(name:String) {
-//    Dashboard("DASHBOARD_SCREEN"),
-//    Welcome("WELCOME_SCREEN"),
-//    WelcomeStart("WELCOME_START_SCREEN"),
-//    WelcomeInstructions("WELCOME_INS_SCREEN"),
-//    RCRCProjects("RCRC_PROJECTS_SCREEN"),
-//    EnterName("ENTER_NAME_SCREEN"),
-//    AboutRCRC("ABOUT_RCRC_SCREEN"),
-//    TemiGuide("TEMI_GUIDE_SCREEN"),
-//    MeetingInfo("MEETING_INFO_SCREEN"),
-//    MeetingNotFound("MEETING_NOT_FOUND_SCREEN")
 
     StartFragment("start-fragment"),
     EnterNameFragment("enter_name"),
